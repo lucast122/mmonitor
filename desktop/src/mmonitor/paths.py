a54741b3a631774c8cd9c4220b8fb840857571de
lucast122 <timolucas1994@gmail.com>
2025-01-24 12:57:36 +0100

    fixed flye path in paths

--- desktop/src/mmonitor/paths.py
+++ desktop/src/mmonitor/paths.py
@@ -1,7 +1,8 @@
 import os
 
-# Get the src directory path
-SRC_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+# Get the project root directory path
+PROJECT_ROOT = "/home/minion-computer/mmonitor-pipeline"
+SRC_DIR = os.path.join(PROJECT_ROOT, "desktop", "src")
 RESOURCES_DIR = os.path.join(SRC_DIR, "resources")
 IMAGES_DIR = os.path.join(RESOURCES_DIR, "images")
-LIB_DIR = os.path.join(SRC_DIR, "lib")
+LIB_DIR = os.path.join(SRC_DIR, "mmonitor", "lib")
